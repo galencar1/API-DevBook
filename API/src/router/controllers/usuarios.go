39cdc423de484c7543a5c75452750892f9ce00ac
Gabriel Alencar <gfalencar02@gmail.com>
2023-01-13 15:32:10 -0300

    implements controllers CRUD

---
+++ API/src/router/controllers/usuarios.go
@@ -0,0 +1,28 @@
+package controllers
+
+import "net/http"
+
+// Criar usuário insere um usuário no banco de dados
+func CriarUsuario(w http.ResponseWriter, r *http.Response) {
+	w.Write([]byte("Criando Usuário!!!"))
+}
+
+// Buscar usuários retorna todos os usuários do banco de dados
+func BuscarUsuarios(w http.ResponseWriter, r *http.Response) {
+	w.Write([]byte("Buscando todos os usuários!!!"))
+}
+
+// Buscar usuário retorna um usuário do banco de dados
+func BuscarUsuario(w http.ResponseWriter, r *http.Response) {
+	w.Write([]byte("Buscando um usuário!!!"))
+}
+
+// Atualizar usuário atualiza um usuário no banco de dados
+func AtualizarUsuario(w http.ResponseWriter, r *http.Response) {
+	w.Write([]byte("Atualizando Usuário!!!"))
+}
+
+// Deletar usuário deleta um usuário do banco de dados
+func DeletarUsuario(w http.ResponseWriter, r *http.Response) {
+	w.Write([]byte("Deletando Usuário!!!"))
+}
