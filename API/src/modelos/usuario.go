8b528fb6a7a82a8aa4d20e0f4b78a4843aa9f1db
Gabriel Alencar <gfalencar02@gmail.com>
2023-01-16 09:40:13 -0300

    corrigindo tipo de dados do modelo de usuario

--- API/src/modelos/usuario.go
+++ API/src/modelos/usuario.go
@@ -5,9 +5,9 @@ import "time"
 // Usuario representa um usuário utilizando a rede social
 type Usuario struct {
 	ID       uint64    `json:"id,omitempty"`
-	Nome     uint64    `json:"nome,omitempty"`
-	Nick     uint64    `json:"nick,omitempty"`
-	Email    uint64    `json:"email,omitempty"`
-	Senha    uint64    `json:"senha,omitempty"`
+	Nome     string    `json:"nome,omitempty"`
+	Nick     string    `json:"nick,omitempty"`
+	Email    string    `json:"email,omitempty"`
+	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"CriadoEm,omitempty"`
 }
